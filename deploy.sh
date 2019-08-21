d532157cd8856d08349602c1dd294ea75eb15d83
OkiMaureen <oki.maureen.eloho@gmail.com>
2019-08-21 20:14:34 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 echo "Starting python installation...."
 {
+  export LC_ALL=C
   sudo apt-get install python-pip -y
   sudo add-apt-repository ppa:deadsnakes/ppa
   sudo apt-get update
@@ -16,27 +17,21 @@ echo "Starting python installation...."
 }
 
 echo "Installing pipenv..."
-{
-	pip3 install pipenv
+{ 
+	pip install pipenv
 	echo "Pipenv installed :)"
 } || {
 	echo "Pipenv installation failed"
 }
 
 echo "Installing server dependencies"
-  pipenv run pipenv install
-  pipenv run python manage.py migrate
-  pipenv run python manage.py loaddata category_fixtures.json
+  pipenv run pipenv install -r  requirements.txt
+  cd favourite_things_api && pipenv run python manage.py migrate && pipenv run python manage.py loaddata category_fixtures.json
 echo "Dependencies installed and defaults added"
 
 echo "Starting gunicorn"
-{
-  sudo apt-get install -y supervisor
-  sudo touch /etc/supervisor/conf.d/gunicorn.conf
-  sudo cp gunicorn.conf /etc/supervisor/conf.d/gunicorn.conf
-  sudo mkdir /var/log/gunicorn
-  sudo supervisorctl reread 
-  sudo supervisorctl update
+{  
+  gunicorn --bind 0.0.0.0:8000 favourite_things_api.wsgi:application --daemon && cd -
   echo "gunicorn running"
 } || {
   echo "gunicorn not running"
@@ -44,7 +39,8 @@ echo "Starting gunicorn"
 
 
 echo "Installing nodejs"
-{   sudo apt-get install curl
+{   
+    sudo apt-get install curl
 	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get install nodejs
 	echo "Node installed :)"
@@ -53,17 +49,28 @@ echo "Installing nodejs"
 }
 
 echo "Installing client dependencies"
-cd favourite_things_client && npm install && cd -
+{
+cd favourite-things-client && npm install && cd -
 echo "Dependencies installed :)"
+} || {
+    echo "Failed"
+}
 
 echo "building client"
-cd favourite_things_client && npm run build && cd -
+{
+cd favourite-things-client && npm run build && cd -
 echo "client built :)"
+} || {
+    echo "Failed client build"
+}
 
 
 echo "Installing Nginx"
-cd /etc/nginx/sites-available
+sudo apt-get install nginx -y
+sudo touch /etc/nginx/sites-available/django.conf
 sudo cp django.conf /etc/nginx/sites-available/django.conf
+sudo ln -s /etc/nginx/sites-available/django.conf /etc/nginx/sites-enabled/django.conf
+
 sudo nginx -t
 sudo service nginx restart
 
